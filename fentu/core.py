ccd19669e1be497b1055a494f6d32b30d33d0853
chenxu10 <shenxu2011@gmail.com>
2021-08-05 16:10:53 -0400

    setup fuckyou money

---
+++ fentu/core.py
@@ -0,0 +1,17 @@
+def buy():
+    pass
+
+def sell():
+    pass
+
+def trade():
+    buy_condition = None
+    if buy_condition:
+        buy()
+    else:
+        sell()
+
+if __name__ == '__main__':
+    trade()
+
+#TODO:understand a classic option strategy 
