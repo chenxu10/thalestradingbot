ddb4b998f9f9c62fd32e4ebde61d4ab1b22e3379
chenxu10 <shenxu2011@gmail.com>
2022-05-23 12:02:57 -0400

    first attempt to test function to FTX

---
+++ tests/test_ftx_connection.py
@@ -0,0 +1,7 @@
+# https://blog.ftx.com/blog/api-authentication/
+# TODO write test function for ftx conncetion
+
+# Historical-data-Basket trader-Your Basket account
+
+def test_ftx_api_connection():
+    raise notImplementedError
