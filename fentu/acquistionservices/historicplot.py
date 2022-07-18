d29d44fe21b10784e0705ec415164513bb02295a
chenxu10 <shenxu2011@gmail.com>
2022-07-18 12:13:11 -0400

    finish sketch running geometric mean

--- fentu/acquistionservices/historicplot.py
+++ fentu/acquistionservices/historicplot.py
@@ -1,26 +1,66 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import gmean
 
-def get_qratio():
+def calculate_geometric_mean(x):
+    ans = []
+    
+    for i in range(1, len(x) + 1):
+        ans.append(gmean(x[i-3:i]))
+
+    ans = [round(s,2) for s in ans]
+
+    return ans
+
+def get_running_gmean_qratio():
     """
     Returns:
+        Running geometric avaerage from 1944 to 2022
         A list of q ratio starting from 1944 to 2022
     """
-    df = pd.read_csv("data/csv/b103.csv")
-    df = df.loc[:,['LM103164103.Q','FL102090005.Q']]
-    df.rename(
-        columns={"LM103164103.Q":"corp_eqt_lblt","FL102090005.Q":"corp_net_worth"},
-        inplace=True)
-    df = df[df["corp_eqt_lblt"].str.contains("ND")==False]
-    df = df[df["corp_net_worth"].str.contains("ND")==False]
-    df['corp_eqt_lblt'] = df['corp_eqt_lblt'].astype(int)
-    df['corp_net_worth'] = df['corp_net_worth'].astype(int)
-    df['q_ratio'] =  df['corp_eqt_lblt']/df['corp_net_worth']
-    df['q_ratio'].plot()
+    def load_data():
+        return pd.read_csv("data/csv/b103.csv")
+
+    def pick_interested_columns(df):
+        df = df.loc[:,['LM103164103.Q','FL102090005.Q']]
+        return df
+
+    def rename_column(df):
+        df = df.rename(
+            columns={
+                "LM103164103.Q":"corp_eqt_lblt",
+                "FL102090005.Q":"corp_net_worth"}
+            )
+        return df
+
+    def delete_null_values(df):
+        for c in ["corp_eqt_lblt","corp_net_worth"]:
+            df = df.loc[df[c].str.contains("ND")==False,:]
+            df[c] = df[c].astype(int)
+        return df
+
+    def calculate_geometric_columns(df):
+        for c in ["corp_eqt_lblt","corp_net_worth"]:
+            df["geo_" + c] = calculate_geometric_mean(list(df[c]))
+        return df
+
+    def add_geo_q_ratio(df):
+        df['geo_q_ratio'] = df['geo_corp_eqt_lblt']/df['geo_corp_net_worth']
+        return df
+
+
+    df =(load_data().
+        pipe(pick_interested_columns).
+        pipe(rename_column).
+        pipe(delete_null_values).
+        pipe(calculate_geometric_columns).
+        pipe(add_geo_q_ratio)
+        )    
+
+    df['geo_q_ratio'].plot()
     plt.show()
     return df
 
 if __name__ == '__main__':
-    get_qratio()
-
+    get_running_gmean_qratio()
 
--- tests/test_acquistion_services.py
+++ tests/test_acquistion_services.py
@@ -1,19 +1,27 @@
 import pandas as pd
-from fentu.acquistionservices.historicplot import get_qratio
+from fentu.acquistionservices.historicplot import get_running_gmean_qratio, calculate_geometric_mean
 
 def test_get_qratio():
     """
     ROIC(return on invested capital)
     """
-    actual = get_qratio()
+    actual = get_running_gmean_qratio()
     print(actual)
     expected = [1 for _ in range(73 * 4 - 4)]
     assert len(actual['q_ratio']) == len(expected)
-    
-def test_plot_qratio():
-    """
-    """
-    raise notImplementedError
+
+def test_calculate_geomtric_mean():
+    input = [1.2,1.3,0.5,0.7]
+    actual = calculate_geometric_mean(input)
+    print(actual)
+    expected = [1.2,1.25,0.92,0.86]
+    assert actual == expected
+
+# def test_plot_qratio():
+#     """
+#     """
+#     raise notImplementedError
 
 if __name__ == '__main__':
-    test_get_qratio()
+    print(test_calculate_geomtric_mean())
+    # test_get_qratio()
