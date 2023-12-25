75b67f873f0b42ac5de40368731339049db15b3e
chenxu10 <shenxu2011@gmail.com>
2023-12-24 20:40:34 -0500

    refactor to make fixtures and opdata global to remove duplication code in test

--- fentu/strategyservices/black_scholes_model.py
+++ fentu/strategyservices/black_scholes_model.py
@@ -11,50 +11,52 @@ class OptionData:
         self.volatility = volatility
 
 class BlackScholesPricer():
+    def __init__(self, opdata) -> None:
+        self.opdata = opdata
+
     def price_empty():
         raise NotImplementedError
-
-    def black_scholes_model_pricing(self, opdata):
-        pass
     
-    def nostrike_change_at_expiration(self, opdata):
+    def nostrike_change_at_expiration(self):
         return 0
     
-    def how_far_asset_price_to_strike_price(self, opdata):
-        ratio_of_spot_and_stock = opdata.spot_price / opdata.strike_price
+    def how_far_asset_price_to_strike_price(self):
+        ratio_of_spot_and_stock = self.opdata.spot_price / self.opdata.strike_price
         distance_between_strike_and_spot = math.log(ratio_of_spot_and_stock)
         return distance_between_strike_and_spot
     
-    def what_is_expected_return_adjusted_for_riskness(self, opdata):
-        return (opdata.interest_rate + 0.5 * (opdata.volatility ** 2)) * opdata.time_to_expire
+    def what_is_expected_return_adjusted_for_riskness(self):
+        return (self.opdata.interest_rate + 0.5 * (self.opdata.volatility ** 2)) * self.opdata.time_to_expire
 
-    def calculate_d1(self, opdata):
+    def calculate_d1(self):
         """
         Calculate how likely option is going to expire in the money based on current
         asset price
         """
-        distance_between_strike_and_spot = self.how_far_asset_price_to_strike_price(opdata)
-        expected_return_adjusted_for_riskness = self.what_is_expected_return_adjusted_for_riskness(opdata)
-        expected_fluctuation_in_the_life_of_option = opdata.volatility * np.sqrt(opdata.time_to_expire)
+        distance_between_strike_and_spot = self.how_far_asset_price_to_strike_price()
+        expected_return_adjusted_for_riskness = self.what_is_expected_return_adjusted_for_riskness()
+        expected_fluctuation_in_the_life_of_option = self.opdata.volatility * np.sqrt(self.opdata.time_to_expire)
         d1 = (distance_between_strike_and_spot + expected_return_adjusted_for_riskness) / expected_fluctuation_in_the_life_of_option
         return d1
     
-    def calculate_d2(self, opdata):
-        d1 = self.calculate_d1(opdata)
-        d2 = d1 - opdata.volatility * np.sqrt(opdata.time_to_expire)
+    def calculate_d2(self):
+        d1 = self.calculate_d1()
+        d2 = d1 - self.opdata.volatility * np.sqrt(self.opdata.time_to_expire)
         return d2
     
-    def calculate_option_price_given(self, opdata, d1, d2):
-        S = opdata.spot_price
-        K = opdata.strike_price
-        r = opdata.interest_rate
-        T = opdata.time_to_expire
-        price = S*norm.cdf(d1) -  K*np.exp(-r*T)*norm.cdf(d2)
+    def calculate_option_price_given(self, d1, d2):
+        S = self.opdata.spot_price
+        K = self.opdata.strike_price
+        r = self.opdata.interest_rate
+        T = self.opdata.time_to_expire
+        expected_stockpricewhenexpired_income = S*norm.cdf(d1)
+        expected_discountedstrikeprice_expense = K*np.exp(-r*T)*norm.cdf(d2)
+        price = expected_stockpricewhenexpired_income - expected_discountedstrikeprice_expense
         return price
 
-    def option_pricing_formula(self, opdata, type="call"):
+    def option_pricing_formula(self, type="call"):
         if type == "call":
-            d1 = self.calculate_d1(opdata)
-            d2 = self.calculate_d2(opdata)
-            call_price = self.calculate_option_price_given(opdata, d1, d2)
+            d1 = self.calculate_d1()
+            d2 = self.calculate_d2()
+            call_price = self.calculate_option_price_given(d1, d2)
             return call_price
--- tests/test_option_pricing.py
+++ tests/test_option_pricing.py
@@ -8,52 +8,53 @@ from scipy.stats import norm
 class TestBlackScholesPricer:
     @pytest.fixture
     def bspricer(self):
-        bspricer = BlackScholesPricer()
+        opdata = OptionData(89,91,30,0.05,0.1)
+        bspricer = BlackScholesPricer(opdata)
         return bspricer
+    
+    @pytest.fixture
+    def bspricer_2(self):
+        opdata = OptionData(42,40,0.5,0.1,0.2)
+        bspricer_2 = BlackScholesPricer(opdata)
+        return bspricer_2
         
     def test_price_empty(self, bspricer):
         with pytest.raises(Exception):
             bspricer.price_empty()
     
     def test_how_far_asset_price_to_strike_price(self, bspricer):
-        opdata = OptionData(89,91,30,0.05,0.1)
         tol = 0.01
-        assert abs(bspricer.how_far_asset_price_to_strike_price(opdata) - (-0.022)) < tol
+        assert abs(bspricer.how_far_asset_price_to_strike_price() - (-0.022)) < tol
     
     def test_what_is_expected_return_adjusted_for_riskness(self, bspricer):
-        opdata = OptionData(89,91,30,0.05,0.1)
         tol = 0.01
-        assert abs(bspricer.what_is_expected_return_adjusted_for_riskness(opdata) - 1.65) < tol
+        assert abs(bspricer.what_is_expected_return_adjusted_for_riskness() - 1.65) < tol
 
     def test_d1(self, bspricer):
-        opdata = OptionData(89,91,30,0.05,0.1)
         tol = 0.01
-        result = bspricer.calculate_d1(opdata)
+        result = bspricer.calculate_d1()
         expected_result = 2.972
         assert abs(result - expected_result) < tol
 
     def test_d2(self, bspricer):
-        opdata = OptionData(89,91,30,0.05,0.1)
         tol = 0.01
-        result = bspricer.calculate_d2(opdata)
+        result = bspricer.calculate_d2()
         expected_result = 2.972 - 0.1 * np.sqrt(30)
         assert abs(result - expected_result) < tol
 
     def test_nostrike_change_at_expiration(self, bspricer):
-        opdata = OptionData(95,95,0,0.041,0.02)
-        assert bspricer.nostrike_change_at_expiration(opdata) == 0
+        assert bspricer.nostrike_change_at_expiration() == 0
     
     def test_calculate_option_price_given(self, bspricer):
-        opdata = OptionData(89,91,30,0.05,0.1)
         tol = 0.01
         d1 = 2.972
         d2 = 2.423
-        result = bspricer.calculate_option_price_given(opdata, d1, d2)
+        result = bspricer.calculate_option_price_given(d1, d2)
         expected_result = 89*norm.cdf(2.972) - 91*np.exp(-0.05*30)*norm.cdf(2.423)
         assert abs(result - expected_result) < tol
 
-    def test_option_pricing_formula(self, bspricer):
-        opdata = OptionData(42,40,0.5,0.1,0.2)
+    @pytest.mark.usefixtures("bspricer_2")
+    def test_option_pricing_formula(self, bspricer_2):
         tol = 0.01
         expected_price = 4.76
-        assert abs(bspricer.option_pricing_formula(opdata,"call") - expected_price) < tol
+        assert abs(bspricer_2.option_pricing_formula("call") - expected_price) < tol
