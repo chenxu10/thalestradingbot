d6ecc4d01ab0c40996bd0f147e21bf5f4a2022bc
chenxu10 <shenxu2011@gmail.com>
2024-01-07 21:03:28 -0500

    working

--- tests/test_call.py
+++ tests/test_call.py
@@ -2,21 +2,20 @@ from fentu.strategyservices.black_scholes_model import OptionData, BlackScholesP
 
 
 def test_call_pnl_base():
-    current_stock_price = 96
     long_strike = 97
     time_to_expire = 3
     interest_rate = 0.05
     volatility = 0.1
     
-    pnl_val = []
+    theortical_value = []
     for s in range(95,105):
         opdata = OptionData(s, long_strike, time_to_expire, 
                         interest_rate, volatility)
         long_call_pricer = BlackScholesPricer(opdata)
         long_call_price_in_stockprice = long_call_pricer.option_pricing_formula("call")
-        pnl_val.append(long_call_price_in_stockprice)
+        theortical_value.append(long_call_price_in_stockprice)
 
-    print(pnl_val)
+    print(theortical_value)
     #assert len(pnl_val) == 10
     #assert pnl_val[0] < pnl_val[-1]
 
