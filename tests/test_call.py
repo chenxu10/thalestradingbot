8ccf34cf4ea47158aab31f5a92397746a9fb7439
chenxu10 <shenxu2011@gmail.com>
2024-01-07 21:00:08 -0500

    working

--- tests/test_call.py
+++ tests/test_call.py
@@ -7,10 +7,15 @@ def test_call_pnl_base():
     time_to_expire = 3
     interest_rate = 0.05
     volatility = 0.1
-    opdata = OptionData(current_stock_price, long_strike, time_to_expire, 
+    
+    pnl_val = []
+    for s in range(95,105):
+        opdata = OptionData(s, long_strike, time_to_expire, 
                         interest_rate, volatility)
-    long_call_pricer = BlackScholesPricer(opdata)
-    pnl_val = [long_call_pricer.option_pricing_formula("call") for s in range(90,110)]
+        long_call_pricer = BlackScholesPricer(opdata)
+        long_call_price_in_stockprice = long_call_pricer.option_pricing_formula("call")
+        pnl_val.append(long_call_price_in_stockprice)
+
     print(pnl_val)
     #assert len(pnl_val) == 10
     #assert pnl_val[0] < pnl_val[-1]
