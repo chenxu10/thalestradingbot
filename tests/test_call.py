db8b2eb1836194bdfa0671c5de027f54605141d1
chenxu10 <shenxu2011@gmail.com>
2024-01-07 20:49:06 -0500

    add test call strategy

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,4 +1,4 @@
-
+from fentu.strategyservices.black_scholes_model import OptionData, BlackScholesPricer
 
 
 def test_call_pnl_base():
@@ -8,8 +8,10 @@ def test_call_pnl_base():
     interest_rate = 0.05
     volatility = 0.1
     price_per_options = 0.35
-
-    pnl_val = [i for i in range(10)]
+    opdata = OptionData(current_stock_price, long_strike, time_to_expire, 
+                        interest_rate, volatility, price_per_options)
+    long_call_pricer = BlackScholesPricer(opdata)
+    pnl_val = [long_call_pricer.option_pricing_formula("call") for s in range(95,105)]
     assert len(pnl_val) == 10
     assert pnl_val[0] < pnl_val[-1]
 
