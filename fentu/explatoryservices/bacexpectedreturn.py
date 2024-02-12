33e1f95a37311e7cb2eab7d63c7af77d04ef80f1
chenxu10 <shenxu2011@gmail.com>
2024-02-12 10:26:19 -0500

    working

--- fentu/explatoryservices/bacexpectedreturn.py
+++ fentu/explatoryservices/bacexpectedreturn.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 
 from fentu.explatoryservices.plotting_service import qq_plot
 
+
+class VolatilityFacade:
+    def __init__(self, instrument):
+        self.daily_returns = self._get_daily_returns(instrument)
+
+    def _get_daily_returns(instrument):
+        instrument = yf.Ticker(instrument)
+        instru_hist = instrument.history(period="max")
+        prices = instru_hist['Close']  
+        returns = prices.pct_change()[1:]
+        return returns
+
+
 def prepare_price_returns(x="BAC"):
     # Download BAC data
     bac = yf.Ticker(x)
