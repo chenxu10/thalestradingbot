bc700b66ce6d8f9de3bbcd7da5bc5b3694c3c59b
chenxu10 <shenxu2011@gmail.com>
2024-03-22 10:03:42 -0400

    visualize yahoo

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from fentu.explatoryservices.plotting_service import qq_plot
+import yfinance as yf
+
+
+
+
+if __name__ == '__main__':
+    x = input("Please input the ETF you want to visualize e.g TLT... ")
+    tltdf = yf.download(x)
+    
