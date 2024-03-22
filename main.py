1ca7533cc84eca399eef749ffe2d767f7e32cf1f
chenxu10 <shenxu2011@gmail.com>
2024-03-22 10:41:09 -0400

    working

--- main.py
+++ main.py
@@ -8,6 +8,5 @@ from fentu.explatoryservices.plotting_service import qq_plot
 if __name__ == '__main__':
     #
     x = input("Please input the ETF you want to visualize e.g TLT... ")
-    df = vc.VolatilityFacade(x)
-    print(df)
-    
+    volatilityofx = vc.VolatilityFacade(x)
+    volatilityofx.visualize_daily_percentage_change()
