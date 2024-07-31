c25bbda39a0ae49d9f414c7180e96d15691d2487
chenxu10 <shenxu2011@gmail.com>
2024-07-31 14:03:57 -0400

    [B]crisis

---
+++ tests/test_crisis_study.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+
+def get_price_in_crisis():
+    tlt = yf.Ticker("TLT")
+    data = tlt.history(start="2020-03-01", end="2020-04-01")
+    return data['Close']
+
+def test_get_price_in_crisis_range():
+    start = "2020-03-01" 
+    end = "2020-04-01"
+    data = get_price_in_crisis()
+    print(data.shape)
+    assert data.shape[0] == 22
+
+def main():
+    test_get_price_in_crisis_range()
+
+if __name__ == '__main__':
+    main()
