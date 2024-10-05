27eab7415e379236cf936e4473a2261c094782c4
chenxu10 <shenxu2011@gmail.com>
2024-10-05 17:35:50 -0400

    working

--- tests/test_calculate_dilute_cost.py
+++ tests/test_calculate_dilute_cost.py
@@ -24,6 +24,10 @@ def calculate_dilueted_cost(x):
     elif status_end_state == "exercised":
         if cur_position_after_option_change == 0:
             new_diluted_cost = strikeprice - preimum / (volume * 100)
+        else:
+            exercised_shares = volume * 100
+            total_buy_cost = strikeprice * exercised_shares + cur_diluted_cost * (cur_position_after_option_change - exercised_shares) 
+            new_diluted_cost = (total_buy_cost - preimum) / cur_position_after_option_change
            
     return new_diluted_cost
 
