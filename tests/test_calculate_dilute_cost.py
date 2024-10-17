06cdd9d90166856dd908d15a431a923d5bde089c
chenxu10 <shenxu2011@gmail.com>
2024-10-17 13:06:40 -0400

    working

--- tests/test_calculate_dilute_cost.py
+++ tests/test_calculate_dilute_cost.py
@@ -4,73 +4,7 @@ for the spot equity you owned
 
 Author: Xu Shen <xs286@cornell.edu>
 """
-import pytest
-
-class DilutedCostCalculator:
-    def __init__(self, data):
-        self.status_end_state = data["end_state"]
-        self.cur_diluted_cost = data["cur_diluted_cost"]
-        self.cur_position_after_option_change = data["cur_position"]
-        self.premium = data["premium"]
-        self.strikeprice = data["strikeprice"]
-        self.volume = data["volume"]
-        self.option_type = data["type"]
-        self.closeprice = data["closeorderprice"]
-
-    def calculate(self):
-        if self.volume < 0:
-            self.volume = abs(self.volume)
-            return self._calculate_new_diluted_cost()
-        return None  # or some default value if volume is not negative
-
-    def _calculate_new_diluted_cost(self):
-        if self.status_end_state == "filled":
-            return self._calculate_filled_diluted_cost()
-        elif self.status_end_state == "expired":
-            return self._calculate_not_exercised_diluted_cost()
-        elif self.status_end_state == "exercised":
-            return self._calculate_exercised_diluted_cost()
-
-    def _calculate_filled_diluted_cost(self):
-        total_buy_cost = self.cur_diluted_cost - self.premium + self.closeprice
-        return total_buy_cost / self.cur_position_after_option_change
-
-    def _calculate_not_exercised_diluted_cost(self):
-        if self.cur_position_after_option_change == 0:
-            return -self.premium
-        return self.cur_diluted_cost - self.premium / self.cur_position_after_option_change
-
-    def _calculate_exercised_diluted_cost(self):
-        exercised_shares = self.volume * 100
-        if self.cur_position_after_option_change == 0:
-            return self.strikeprice - self.premium / (self.volume * 100)
-
-        total_buy_cost = self._calculate_total_buy_cost(exercised_shares)
-        return (total_buy_cost - self.premium) / self.cur_position_after_option_change
-
-    def _calculate_total_buy_cost(self, exercised_shares):
-        if self.option_type == "put":
-            return (self.strikeprice * exercised_shares +
-                    self.cur_diluted_cost * (self.cur_position_after_option_change - exercised_shares))
-        elif self.option_type == "call":
-            return (self.cur_diluted_cost * (self.cur_position_after_option_change + exercised_shares) -
-                    self.strikeprice * exercised_shares)
-
-def test_calculate_expired_order():
-    expired_order_data = {
-        "cur_diluted_cost":100,
-        "cur_position":200,
-        "type":"call",
-        "volume":-1,
-        "end_state":"expired",
-        "premium":20,
-        "strikeprice":90,
-        "closeorderprice":0
-    } 
-    calculator = DilutedCostCalculator(expired_order_data)
-    actual = calculator.calculate()                                                        
-    expected = 99.9
-    assert actual == expected  
+from fentu.accountingservices import calculate_stock_dilute_cost as csdc
 
 def test_calculate_exercised_order():
     exercised_order_data = {
@@ -83,7 +17,7 @@ def test_calculate_exercised_order():
         "strikeprice":90,
         "closeorderprice":90
     }
-    calculator = DilutedCostCalculator(exercised_order_data)
+    calculator = csdc.DilutedCostCalculator(exercised_order_data)
     actual = calculator.calculate()
     expected = 89.8
     assert actual == expected
@@ -99,11 +33,9 @@ def test_calculate_filled_order():
         "strikeprice":90,
         "closeorderprice":9
     }
-    calculator = DilutedCostCalculator(put_filled_order_data)
+    calculator = csdc.DilutedCostCalculator(put_filled_order_data)
     actual = calculator.calculate()
-    print(actual)
     expected = (100 - 10 + 9)/100
-    print(expected)
     assert actual == expected
 
 def test_calculate_dilute_cost():
@@ -118,7 +50,7 @@ def test_calculate_dilute_cost():
         "closeorderprice":90
 
     }
-    calculator = DilutedCostCalculator(put_exercised_order_data)
+    calculator = csdc.DilutedCostCalculator(put_exercised_order_data)
     actual = calculator.calculate()
     expected = 96.6
     assert actual == expected
@@ -135,7 +67,7 @@ def main():
         "strikeprice":36,
         "closeorderprice":0
     }
-    calculator = DilutedCostCalculator(to_calculate_order)
+    calculator = csdc.DilutedCostCalculator(to_calculate_order)
     di = calculator.calculate()
     print(di)
 
