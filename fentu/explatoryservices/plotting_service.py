fb6aca20df64257dd20b8f8c53b50561c49ef800
chenxu10 <shenxu2011@gmail.com>
2024-10-17 13:12:14 -0400

    working

--- fentu/explatoryservices/plotting_service.py
+++ fentu/explatoryservices/plotting_service.py
@@ -9,14 +9,20 @@ import fentu.constants as const
 def calculate_four_moments(x):
     mean = x.mean()
     std = x.std()
+    mad = x.mad()
     skew = x.skew()
     kurtosis = x.kurtosis()
-    return mean, std, skew, kurtosis
+    return mean, mad, std, skew, kurtosis
 
 def qq_plot(x):
     fig = probplot(x, plot=plt)
-    mean, std, skew, kurtosis = calculate_four_moments(x)
-    plt.text(0.05, 0.7, f'Mean: {mean:.4f}\nSD: {std:.4f}\nSkew: {skew:.4f}\nKurtosis: {kurtosis:.4f}', 
+    mean, std, mad, skew, kurtosis = calculate_four_moments(x)
+    plt.text(0.05, 0.7, 
+             f'Mean: {mean:.4f}\
+             \nSD: {std:.4f}\
+             \nMAD:{mad:.4f}\
+             \nSkew: {skew:.4f}\
+             \nKurtosis: {kurtosis:.4f}', 
          transform=plt.gca().transAxes, fontsize=12)
     plt.show()
     return fig
