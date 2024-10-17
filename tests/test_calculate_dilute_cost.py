f66fff2bb112a05da30539001258d601024b2139
chenxu10 <shenxu2011@gmail.com>
2024-10-17 08:25:55 -0400

    working

--- tests/test_calculate_dilute_cost.py
+++ tests/test_calculate_dilute_cost.py
@@ -91,13 +91,13 @@ def test_calculate_dilute_cost():
 def main():
     test_calculate_dilute_cost()
     put_fxi_order = {
-        "cur_diluted_cost":33.81,
-        "cur_position":512,
+        "cur_diluted_cost":33.89,
+        "cur_position":612,
         "type":"put",
-        "volume":2,
+        "volume":1,
         "end_state":"exercised",
-        "preimum":198.64,
-        "strikeprice":35,
+        "preimum":115.32,
+        "strikeprice":36,
         "closeorderprice":0
     }
     di = calculate_dilueted_cost(put_fxi_order)
