96f4269011f178fa494ce13240e45e829236896f
chenxu10 <shenxu2011@gmail.com>
2024-10-25 11:51:46 -0400

    working

--- fentu/explatoryservices/plotting_service.py
+++ fentu/explatoryservices/plotting_service.py
@@ -35,12 +35,16 @@ def calculate_within_onestrd_prop(data):
     print("mean close price change is {}".format(mean))
     print("1 standard deviation close close price change is {}".format(std))
 
-def histgram_plot(data):
-    calculate_within_onestrd_prop(data)
-    sns.histplot(data, x='Close', kde=True, bins=50)
-    close_price = list(data['Close'])
+def fit_with_normal(close_price):
     mu, sigma = stats.norm.fit(close_price)
     x = np.linspace(min(close_price), max(close_price), 100)
     fitted_data = stats.norm.pdf(x, mu, sigma)
     plt.plot(x, fitted_data, 'r-', lw=2, label=f'Normal Distribution\n(μ={mu:.2f}, σ={sigma:.2f})')
+
+def histgram_plot(data):
+    calculate_within_onestrd_prop(data)
+    sns.histplot(data, x='Close', kde=True, bins=50)
+    close_price = list(data['Close'])
+    fit_with_normal(close_price)
     plt.show()
+
