cb0717dc3188f1191ac6369399a1f5d0bed5acd8
chenxu10 <shenxu2011@gmail.com>
2024-10-26 15:47:16 -0400

    working

--- tests/test_loss_cost_model.py
+++ tests/test_loss_cost_model.py
@@ -1,7 +1,17 @@
+"""
+loss cost model
+GBM to loss cost model
+build the bridge between car accidents to stock losses
+"""
+
 import numpy as np
 
-def simulate_total_loss(n, avg_severity, severity_std):
+def simulate_total_loss(n, avg_severity, severity_std, avg_freq, number_of_policies):
     total_losses = np.zeros(n)
+
+    for i in range(n):
+        number_of_accidents = np.random.poisson(
+            avg_freq * number_of_policies)
     
     print(total_losses)
     return 2000
@@ -10,7 +20,10 @@ def test_lcm_model():
     n = 10000
     avg_severity = 5000
     severity_std = 2000
-    total_loss = simulate_total_loss(n, avg_severity, severity_std)
+    avg_freq = 0.1
+    number_of_policies = 100000
+    total_loss = simulate_total_loss(
+        n, avg_severity, severity_std, avg_freq, number_of_policies)
     expected = 2000
     assert total_loss == expected
 
