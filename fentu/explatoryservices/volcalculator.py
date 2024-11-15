6a1141f8062f283d20db8cc63f271e956f08d864
chenxu10 <shenxu2011@gmail.com>
2024-11-14 20:44:13 -0500

    working

--- fentu/explatoryservices/volcalculator.py
+++ fentu/explatoryservices/volcalculator.py
@@ -97,11 +97,11 @@ class VolatilityFacade:
         print(self.daily_returns.tail(20))
 
 if __name__ == "__main__":
-    volatility = VolatilityFacade("FXI")
+    volatility = VolatilityFacade("msft")
     #print(volatility.find_worst_k_days(k=15))
     #volatility.show_today_return()
-    #volatility.visualize_daily_percentage_change()
-    volatility.visualize_weekly_percentage_change()
+    volatility.visualize_daily_percentage_change()
+    #volatility.visualize_weekly_percentage_change()
     #volatility.visualize_monthly_percentage_change()
     #print(volatility.find_worst_months(threshold=-0.3))    
     #print(volatility.find_worst_k_months(k=20))
