290a9f7a3eb1cf35f4d9ef9f8650390d130cd23b
chenxu10 <shenxu2011@gmail.com>
2024-12-08 09:03:34 -0500

    working

--- fentu/explatoryservices/volcalculator.py
+++ fentu/explatoryservices/volcalculator.py
@@ -112,7 +112,7 @@ class VolatilityFacade:
         print(self.daily_returns.tail(20))
 
 if __name__ == "__main__":
-    volatility = VolatilityFacade("TLT")
+    volatility = VolatilityFacade("UVXY")
     #print(volatility.find_worst_k_days(k=15))
     #volatility.show_today_return()
     #volatility.visualize_daily_percentage_change()
