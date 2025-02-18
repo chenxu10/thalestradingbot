99419c402d1eb1a2de825ef1ed0f9d2688e0e5ed
chenxu10 <shenxu2011@gmail.com>
2025-02-18 09:27:33 -0500

    working

--- fentu/explatoryservices/volcalculator.py
+++ fentu/explatoryservices/volcalculator.py
@@ -224,14 +224,14 @@ def taleb_result3_put(S, K, T, r, sigma, liquidity_adj=0.0, jump_risk=0.0):
 
 if __name__ == "__main__":
     volatility = VolatilityFacade("QQQ")
-    #volatility.visualize_weekly_percentage_change()
+    volatility.visualize_weekly_percentage_change()
     #volatility.calculate_daily_volatility()
     #volatility.visualize_monthly_percentage_change()
     #volatility.visualize_yearly_percentage_change()
     #volatility.visualize_daily_percentage_change()
 
     #volatility.show_today_return()
-    print(volatility.find_worst_k_days(k=20))
+    #print(volatility.find_worst_k_days(k=20))
     #print(volatility.find_worst_months(threshold=-0.3))    
     #print(volatility.find_worst_k_months(k=30))
     
