8cd6a6d0c29ae9af021e8e748f21bdf19c078702
chenxu10 <shenxu2011@gmail.com>
2025-03-07 15:14:49 -0500

    working

--- fentu/explatoryservices/volcalculator.py
+++ fentu/explatoryservices/volcalculator.py
@@ -327,6 +327,7 @@ if __name__ == "__main__":
 
     prob_assign2 = black_scholes_prob(S0, K_sell, T, r, sigma)
     prob_otm_assign = black_scholes_prob(S0, K_buy, T, r, sigma)
+    print("heizhang is {}".format(prob_assign2 - prob_otm_assign))
     pnl_assign_range = (70-83) * (prob_assign2 - prob_otm_assign) * 100 + 100
     print("pnl assign range is {}".format(pnl_assign_range))
 
