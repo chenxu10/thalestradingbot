5edf3f5aeae7a15137ddfefe762246e5877b0bb7
chenxu10 <shenxu2011@gmail.com>
2025-03-07 14:49:28 -0500

    working

--- fentu/explatoryservices/volcalculator.py
+++ fentu/explatoryservices/volcalculator.py
@@ -328,7 +328,7 @@ if __name__ == "__main__":
 
     prob_assign2 = black_scholes_prob(S0, K_sell, T, r, sigma)
     prob_otm_assign = black_scholes_prob(S0, K_buy, T, r, sigma)
-    pnl_assign_range = (100 - (K_buy - K_sell)) * (prob_assign2 - prob_otm_assign)
+    pnl_assign_range = (70-83) * (prob_assign2 - prob_otm_assign) + 100
     print(pnl_assign_range)
     # ltweekplotter = LeftTailWeeklyReturnPlotter("TQQQ")
     # ltweekplotter.plot()
