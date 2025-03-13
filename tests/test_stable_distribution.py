2e236a2c2647d414fa7287c53867050671c81e96
chenxu10 <shenxu2011@gmail.com>
2025-03-13 12:02:16 -0400

    working

--- tests/test_stable_distribution.py
+++ tests/test_stable_distribution.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-from fentu.explatoryservices.levy_stable_tail import fit_stable_distribution, plot_stable_fit
+from fentu.explatoryservices.levy_process_alpha_stable import fit_stable_distribution, plot_stable_fit
 
 def test_fit_stable_distribution():
     weekly_changes = [-0.13,-0.10,-0.07,-0.10,-0.14,-0.14]
