977d25c9f3dbff5abdff5abc73df14b854a0b988
chenxu10 <shenxu2011@gmail.com>
2025-03-27 09:38:03 -0400

    working

--- tests/test_vix.py
+++ tests/test_vix.py
@@ -11,7 +11,7 @@ def calculate_uvxy_otm_put_exp_return(uvxy_year_change, option_ticket):
     current_market_price = option_ticket.current_market_price
     strike_price = option_ticket.strike_price
     year_end_return = [
-        max(strike_price - current_market_price * (1 + i),0) for i in uvxy_year_change
+        max(strike_price - current_market_price * (1 + i),0) * 100 for i in uvxy_year_change
     ]
     print(year_end_return)
     return 4000
