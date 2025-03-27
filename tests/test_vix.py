db7e8c3cd6e0db22ffc079207b2b10b292dedbd1
chenxu10 <shenxu2011@gmail.com>
2025-03-26 21:12:06 -0400

    [S]order of function change

--- tests/test_vix.py
+++ tests/test_vix.py
@@ -12,9 +12,10 @@ def calculate_uvxy_otm_put(uvxy_year_change, option_ticket):
 
 def test_calculate_uvxy_otm_put():
     option_ticket = OptionTicket('Put','2026-01-01',3)
+    principle = 10000
     uxvy_year_expected_return = calculate_uvxy_otm_put(
     [-0.5,0.2,0.1],option_ticket)
-    principle = 10000
+    
     assert 0.2 < uxvy_year_expected_return/principle < 0.5
 
 if __name__ == "__main__":
