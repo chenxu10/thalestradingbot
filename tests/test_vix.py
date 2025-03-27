15fe4f7fe1aee8c1d5ac7dd4ab9a6db6f3ae7377
chenxu10 <shenxu2011@gmail.com>
2025-03-26 21:15:53 -0400

    working

--- tests/test_vix.py
+++ tests/test_vix.py
@@ -7,14 +7,14 @@ class OptionTicket:
     strike_price: float
     current_underlying_price: float
 
-def calculate_uvxy_otm_put(uvxy_year_change, option_ticket):
+def calculate_uvxy_otm_put_exp_return(uvxy_year_change, option_ticket):
     return 4000
 
 
 def test_calculate_uvxy_otm_put():
     option_ticket = OptionTicket('Put','2026-01-01',3, 20.57)
     principle = 10000
-    uxvy_year_expected_return = calculate_uvxy_otm_put(
+    uxvy_year_expected_return = calculate_uvxy_otm_put_exp_return(
     [-0.5,0.2,0.1],option_ticket)
     
     assert 0.2 < uxvy_year_expected_return/principle < 0.5
