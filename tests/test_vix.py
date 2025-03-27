c6d66a9b4f16466caad0650167266111c269f556
chenxu10 <shenxu2011@gmail.com>
2025-03-26 21:11:35 -0400

    working

--- tests/test_vix.py
+++ tests/test_vix.py
@@ -9,9 +9,13 @@ class OptionTicket:
 def calculate_uvxy_otm_put(uvxy_year_change, option_ticket):
     return 4000
 
-option_ticket = OptionTicket('Put','2026-01-01',3)
-uxvy_year_expected_return = calculate_uvxy_otm_put(
-    [-0.5,0.2,0.1],option_ticket)
-principle = 10000
-assert 0.2 < uxvy_year_expected_return/principle < 0.5
 
+def test_calculate_uvxy_otm_put():
+    option_ticket = OptionTicket('Put','2026-01-01',3)
+    uxvy_year_expected_return = calculate_uvxy_otm_put(
+    [-0.5,0.2,0.1],option_ticket)
+    principle = 10000
+    assert 0.2 < uxvy_year_expected_return/principle < 0.5
+
+if __name__ == "__main__":
+    test_calculate_uvxy_otm_put()
