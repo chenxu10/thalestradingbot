188848af1aa441fe02855b9b6985f99de391a29d
chenxu10 <shenxu2011@gmail.com>
2025-03-26 21:14:36 -0400

    working

--- tests/test_vix.py
+++ tests/test_vix.py
@@ -5,13 +5,14 @@ class OptionTicket:
     option_type: str
     expiration: str
     strike_price: float
+    current_underlying_price: float
 
 def calculate_uvxy_otm_put(uvxy_year_change, option_ticket):
     return 4000
 
 
 def test_calculate_uvxy_otm_put():
-    option_ticket = OptionTicket('Put','2026-01-01',3)
+    option_ticket = OptionTicket('Put','2026-01-01',3, 20.57)
     principle = 10000
     uxvy_year_expected_return = calculate_uvxy_otm_put(
     [-0.5,0.2,0.1],option_ticket)
