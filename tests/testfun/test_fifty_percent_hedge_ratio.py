8792d716d73f3ab1c82ef657d8a433d9428ea68a
chenxu10 <shenxu2011@gmail.com>
2025-06-22 09:51:05 -0400

    working

--- tests/testfun/test_fifty_percent_hedge_ratio.py
+++ tests/testfun/test_fifty_percent_hedge_ratio.py
@@ -6,11 +6,11 @@ def test_explore_qixian_cuopei():
         """
         Expire at 09/30 Strike at 473
         """
-        return 473 / 70
+        return 473 / 14
     
     three_months_daily_option_cost = calculate_three_months_daily_option_cost()
     print(three_months_daily_option_cost)
-    daily_option_preimum = 7
+    daily_option_preimum = 100
 
     assert three_months_daily_option_cost < daily_option_preimum
 
