2ad06a56ff744854d8998293a863c747125567b2
chenxu10 <shenxu2011@gmail.com>
2026-08-07 17:37:47 -0400

    xingjian: working 17:37:47

--- tests/test_tail_ratio_percentile.py
+++ tests/test_tail_ratio_percentile.py
@@ -1,11 +1,11 @@
 from fentu.pricingservices.tail_ratio import percentile
 
+ANNUAL_RATIOS_2016_2026 = [0.05 + 0.01 * i for i in range(10)]
+
 
 def test_percentile_25th_known_series():
-    series = [0.05 + 0.01 * i for i in range(10)]  # 10 annual ratios, 2016-2026
-    assert round(percentile(series, 25), 4) == 0.0725
+    assert round(percentile(ANNUAL_RATIOS_2016_2026, 25), 4) == 0.0725
 
 
 def test_percentile_75th_known_series():
-    series = [0.05 + 0.01 * i for i in range(10)]  # 10 annual ratios, 2016-2026
-    assert round(percentile(series, 75), 4) == 0.1175
+    assert round(percentile(ANNUAL_RATIOS_2016_2026, 75), 4) == 0.1175
