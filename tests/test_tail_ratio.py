d133dedb0783fbead0d8b66923823715a5974557
chenxu10 <shenxu2011@gmail.com>
2026-08-07 17:29:29 -0400

    xingjian: working 17:29:29

--- tests/test_tail_ratio.py
+++ tests/test_tail_ratio.py
@@ -2,4 +2,4 @@ from fentu.pricingservices.tail_ratio import wing_to_body_ratio
 
 
 def test_wing_to_body_ratio_known_case():
-    assert round(wing_to_body_ratio(4.46, 69.83), 4) == 0.0639
+    assert round(wing_to_body_ratio(4.46, 69.83), 3) == 0.064
